c83f5b75c811a6b57f65e57acbbd2133d7a60fda
Eduardo Rojas <carlos.edhr@protonmail.com>
2026-01-08 13:48:38 -0800

    feat: implemented REST API with django rest framework, API includes access with authentication and custom permissions to courses and module contents

--- calmecac/settings.py
+++ calmecac/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "embed_video",
     "debug_toolbar",
     "redisboard",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -148,3 +149,10 @@ CACHE_MIDDLEWARE_ALIAS = "default"
 CACHE_MIDDLEWARE_SECONDS = 60 * 15
 # 15 minutes
 CACHE_MIDDLEWARE_KEY_PREFIX = "calmecac"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
